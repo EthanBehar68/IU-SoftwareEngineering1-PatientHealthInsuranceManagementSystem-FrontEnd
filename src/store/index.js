d37023fc1498234773cc616b23579110ba6ba5f6
Jack Joliet <jrjoliet@iu.edu>
2020-09-22 18:11:31 -0400

    add redux devtools line

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,8 @@ export function configureStore(){
 	const store = createStore(
 		rootReducer, 
 		compose(
-			applyMiddleware(thunk)
+			applyMiddleware(thunk),
+			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 		)
 	);
 
