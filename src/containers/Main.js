ca7b31850a636ec1202a4079e79a4bdce32ad42a
Jack Joliet <jrjoliet@iu.edu>
2020-09-24 16:29:48 -0400

    Initial theme, material-ui setup, and navbar

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,7 +1,14 @@
 import React, {Component, Fragment} from 'react';
-import {Switch, Route, withRouter, Redirect} from 'react-router-dom';
+import {Route, withRouter, Redirect} from 'react-router-dom';
 import PrivateRoute from '../components/Nav/PrivateRoute';
 import {connect} from "react-redux";
+
+import {Grid, withWidth, withTheme, ThemeProvider} from '@material-ui/core';
+import {createMuiTheme} from '@material-ui/core/styles';
+
+import {state_set_dark} from '../store/actions/dark';
+
+import Navbar from '../components/Nav/Navbar';
 
 class Main extends Component {
 	constructor(props) {
@@ -12,17 +19,44 @@ class Main extends Component {
 	}
 
 	render() {
+		const {width, dark} = this.props;
+
+		const small = width === "xs" || width === "sm";
+		const xs = width === "xs";
+
+		const maxWidth = small ? "100%" : "60rem";
+
+		const theme = createMuiTheme({
+		  typography: {
+		    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+		  },
+		  palette: {
+		    primary: {
+		      main: dark ? "#fff" : "#002868",
+		      contrastText: dark ? "#002868" : "#fff"
+		    },
+		    secondary: {
+		      main: dark ? "#fff" : "#bf0a30",
+		      contrastText: dark ? "#bf0a30" : "#fff"
+		    },
+		    background: {
+		      main: dark ? "#002868" : "#fff"
+		    }
+		  }
+		});
 
 		return (
-			<Fragment>
-				Hello World
-      </Fragment>
+			<ThemeProvider theme={theme}>
+				<Grid container item direction="column" xs={12}>
+					<Navbar maxWidth={maxWidth} xs={xs} small={small} theme={theme.palette} dark={dark} state_set_dark={this.props.state_set_dark}/>
+	      </Grid>
+      </ThemeProvider>
 		)
 	}
 }
 
 const mapStateToProps = state => ({
-
+	dark: state.dark
 });
 
-export default connect(mapStateToProps,{})(withRouter(Main));
+export default connect(mapStateToProps,{state_set_dark})(withRouter(withWidth()(Main)));
