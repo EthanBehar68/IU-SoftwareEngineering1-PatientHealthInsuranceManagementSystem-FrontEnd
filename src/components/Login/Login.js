6a0aaf0798cae55075339847e5bb972dc752ef48
Jack Joliet <jrjoliet@iu.edu>
2020-09-29 11:10:01 -0400

    registration page and google oauth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,19 +2,19 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from "react-redux";
 import empty from 'is-empty';
+import ReCAPTCHA from "react-google-recaptcha";
+import { GoogleLogin } from 'react-google-login';
 
 import {get_role_img} from '../../utils/images';
 
-import {Grid, TextField, Button} from '@material-ui/core';
-
-import ReCAPTCHA from "react-google-recaptcha";
+import {Grid, TextField, Button, Divider} from '@material-ui/core';
 
 class Login extends Component {
 	constructor(props) {
 		super(props);
     this.state = {
       data: {
-        username: '',
+        email: '',
         password: ''
       },
       captcha: false
@@ -33,7 +33,9 @@ class Login extends Component {
 
   render() {
   	const {small, xs, theme, dark, role} = this.props;
-    const {data} = this.state;
+    const {data, captcha} = this.state;
+
+    const emptyCheck = !empty(data.email) && !empty(data.password) && captcha;
 
   	return(
   		<Grid xs={12} container item style={{borderRadius: 6, border: `1px solid ${theme.primary.main}`, padding: (dark && small) ? "0" : "1.5rem"}}>
@@ -41,18 +43,19 @@ class Login extends Component {
           <img src={get_role_img(role)} alt="" style={{width: "100%", objectFit: "cover", height: "100%", borderRadius: 6}}/>
         </Grid>)}
         <Grid sm={12} md={6} container item direction="column" style={{background: "#fff", padding: dark ? "1rem" : "", borderRadius: 6}}>
-          <span style={{color: !dark ? theme.primary.main : theme.primary.contrastText, fontSize: "2rem", lineHeight: "1.6rem", marginBottom: "1rem"}}>{role.toUpperCase()} LOGIN</span>
+          <span style={{color: !dark ? theme.primary.main : theme.primary.contrastText, fontSize: "2rem", lineHeight: "1.6rem", marginBottom: "1rem", fontWeight: 500}}>{role.toUpperCase()} LOGIN</span>
           <TextField
             size="small"
             variant="outlined"
-            value={data.username}
-            name="username"
+            value={data.email}
+            name="email"
             onChange={this.handleDataChange}
             color="primary"
             inputProps={{
-              placeholder: "Username"
+              placeholder: "Email"
             }}
             style={{marginBottom: "0.5rem"}}
+            fullWidth
           />
           <TextField
             size="small"
@@ -66,14 +69,29 @@ class Login extends Component {
               placeholder: "Password"
             }}
             style={{marginBottom: "0.5rem"}}
+            fullWidth
           />
           <Link to="/forgot" style={{marginBottom: "0.5rem", color: !dark ? theme.primary.main : theme.primary.contrastText, fontSize: "0.9rem"}}>Forgot Password?</Link>
           <ReCAPTCHA
             sitekey="6LdAldEZAAAAANrCrYaxC6PpkQBYcmOASFJjXht1"
-            style={{marginBottom: "0.5rem"}}
             onChange={value => this.setState({...this.state, captcha: !empty(value)})}
+            style={{marginBottom: "0.5rem"}}
           />
-          <Button variant="contained" style={{backgroundColor: "#002868", color: "white"}} size="large">Login</Button>
+          <Button variant="contained" fullWidth style={{backgroundColor: !emptyCheck ? "" : "#002868", color: !emptyCheck ? "" : "white", marginBottom: "1rem"}} size="large" disabled={emptyCheck}>Login</Button>
+          <GoogleLogin
+            clientId="669027125073-9hm16et2k1jq5jgockuo1h88ff1sjc47.apps.googleusercontent.com"
+            buttonText="Login with Google"
+            onSuccess={this.onGoogleSuccess}
+            onFailure={this.onGoogleFailure}
+            cookiePolicy={'single_host_origin'}
+          />
+          <div style={{height: "1rem"}}/>
+          <div style={{display: "flex", alignItems: "center", marginBottom: "1rem", width: "100%"}}>
+            <Divider style={{flex: 1, marginRight: "1rem"}}/>
+            <span style={{fontSize: "0.9rem", color: "#002868"}}>or</span>
+            <Divider style={{flex: 1, marginLeft: "1rem"}}/>
+          </div>
+          <Link to={`/register/${role}`} style={{width: "100%"}}><Button variant="contained" fullWidth style={{backgroundColor: "#002868", color: "white"}} size="large">Register</Button></Link>
         </Grid>
   		</Grid>
   	);
--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -1,22 +1,139 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from "react-redux";
+import empty from 'is-empty';
+import ReCAPTCHA from "react-google-recaptcha";
 
-import {Grid} from '@material-ui/core';
+import {get_role_img} from '../../utils/images';
+
+import {Grid, TextField, Button, Divider} from '@material-ui/core';
+
+import PhoneInput from '../Inputs/PhoneInput';
 
 class Register extends Component {
 	constructor(props) {
 		super(props);
+    this.state = {
+      data: {
+      	first: '',
+      	last: '',
+        email: '',
+        phone: '',
+        password: ''
+      },
+      captcha: false
+    }
 	}
 
+  handleDataChange = e => {
+    this.setState({
+      ...this.state,
+      data: {
+        ...this.state.data,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
   render() {
-  	const {maxWidth, small, xs, theme, dark} = this.props;
+  	const {small, xs, theme, dark, role} = this.props;
+    const {data, captcha} = this.state;
+
+    const emptyCheck = !empty(data.first) && !empty(data.last) && !empty(data.email) && !empty(data.phone) && !empty(data.password) && captcha;
 
   	return(
-  		<Grid xs={12} container item direction="column" alignItems="center" style={{background: theme.background.main}}>
-  			Register
+  		<Grid xs={12} container item style={{borderRadius: 6, border: `1px solid ${theme.primary.main}`, padding: (dark && small) ? "0" : "1.5rem"}}>
+  			{!small && (<Grid sm={0} md={4} container item style={{paddingRight: small ? "" : "1.5rem"}}>
+          <img src={get_role_img(role)} alt="" style={{width: "100%", objectFit: "cover", height: "100%", borderRadius: 6}}/>
+        </Grid>)}
+        <Grid sm={12} md={8} container item direction="column" style={{background: "#fff", padding: dark ? "1rem" : "", borderRadius: 6}}>
+          <span style={{color: !dark ? theme.primary.main : theme.primary.contrastText, fontSize: "2rem", lineHeight: "1.6rem", marginBottom: "1rem", fontWeight: 500}}>{role.toUpperCase()} REGISTRATION</span>
+          <Grid item container xs={12}>
+	          <Grid item container xs={12} sm={6} style={{paddingRight: !xs ? "0.25rem" : ""}}>
+	          	<TextField
+		            size="small"
+		            variant="outlined"
+		            value={data.first}
+		            name="first"
+		            onChange={this.handleDataChange}
+		            color="primary"
+		            inputProps={{
+		              placeholder: "First Name"
+		            }}
+		            style={{marginBottom: "0.5rem"}}
+		            fullWidth
+		          />
+	          	<TextField
+		            size="small"
+		            variant="outlined"
+		            value={data.email}
+		            name="email"
+		            onChange={this.handleDataChange}
+		            color="primary"
+		            inputProps={{
+		              placeholder: "Email"
+		            }}
+		            style={{marginBottom: "0.5rem"}}
+		            fullWidth
+		          />
+	          </Grid>
+	          <Grid item container xs={12} sm={6} style={{paddingLeft: !xs ? "0.25rem" : ""}}>
+	          	<TextField
+		            size="small"
+		            variant="outlined"
+		            value={data.last}
+		            name="last"
+		            onChange={this.handleDataChange}
+		            color="primary"
+		            inputProps={{
+		              placeholder: "Last Name"
+		            }}
+		            style={{marginBottom: "0.5rem"}}
+		            fullWidth
+		          />
+		          <PhoneInput
+		          	value={data.phone}
+		          	name="phone"
+		          	onChange={value => this.setState({...this.state, data: {...data, phone: value}})}
+		          	style={{marginBottom: "0.5rem"}}
+		          />
+	          </Grid>
+          </Grid>
+          <TextField
+            size="small"
+            variant="outlined"
+            value={data.password}
+            name="password"
+            type="password"
+            color="primary"
+            onChange={this.handleDataChange}
+            inputProps={{
+              placeholder: "Password"
+            }}
+            style={{marginBottom: "0.5rem"}}
+            fullWidth
+          />
+          <ReCAPTCHA
+            sitekey="6LdAldEZAAAAANrCrYaxC6PpkQBYcmOASFJjXht1"
+            style={{marginBottom: "0.5rem"}}
+            onChange={value => this.setState({...this.state, captcha: !empty(value)})}
+          />
+          <Button variant="contained" fullWidth style={{backgroundColor: !emptyCheck ? "" : "#002868", color: !emptyCheck ? "" : "white", marginBottom: "1rem"}} size="large" disabled={emptyCheck}>Register</Button>
+          <div style={{display: "flex", alignItems: "center", marginBottom: "1rem", width: "100%"}}>
+            <Divider style={{flex: 1, marginRight: "1rem"}}/>
+            <span style={{fontSize: "0.9rem", color: "#002868"}}>or</span>
+            <Divider style={{flex: 1, marginLeft: "1rem"}}/>
+          </div>
+          <Link to={`/login/${role}`} style={{width: "100%"}}><Button variant="contained" fullWidth style={{backgroundColor: "#002868", color: "white"}} size="large">Login</Button></Link>
+        </Grid>
   		</Grid>
   	);
   }
 }
+
+const mapStateToProps = state => ({
+  dark: state.dark
+});
  
-export default Register;
+export default connect(mapStateToProps,{})(Register);
+
