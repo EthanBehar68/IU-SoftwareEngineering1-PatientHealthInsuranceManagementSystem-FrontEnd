2c35ac9029068b866db800c522bdc3104ae87de7
Jack Joliet <jrjoliet@iu.edu>
2020-11-03 15:13:25 -0500

    remove dark mode for now

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec0747fd77627950ea60b1f51406e571.js"
+  "/precache-manifest.aaa972271806da60c271ad68fff2a488.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/components/Nav/Footer.js
+++ src/components/Nav/Footer.js
@@ -27,16 +27,20 @@ class Footer extends Component {
   	return(
   		<Grid xs={12} container item direction="column" alignItems="center" style={{background: theme.background.main, zIndex: 100}}>
   			<Divider style={{background: theme.primary.main, opacity: 0.4, width: "100%"}}/>
-  			<div style={{zIndex: 99, maxWidth: maxWidth, width: small ? "calc(100% - 2rem)" : "100%", display: "flex", alignItems: "center", justifyContent: "space-between", padding: small ? "1rem" : "0.4rem"}}> 
+  			<div style={{zIndex: 99, maxWidth: maxWidth, width: small ? "calc(100% - 2rem)" : "100%", display: "flex", alignItems: "center", justifyContent: "space-between", padding: small ? "1rem" : "0.4rem 0", height: "2rem"}}> 
+	  			{/*
 	  			<Grid xs={12} container item alignItems="center" style={{padding: !small ? "0.5rem 0" : "", paddingLeft: !small ? "" : "0.5rem", marginLeft: "-1rem", flex: 1}}>
+			  		
 			  		<Switch
 			        checked={dark}
 			        onChange={this.setTheme}
 			        color="primary"
-			      />
+			      /> 
 			      <span style={{marginLeft: "0.3rem", color: theme.primary.main}}>{dark ? 'Dark' : "Light"}</span>
 		      </Grid>
-		      <span style={{fontWeight: 300, fontSize: "0.8rem", flex: 1, textAlign: "right", paddingTop: "0.2rem", color: theme.primary.main}}>APOLLOCARE 2020 &copy;</span>
+		      */
+			    }
+		      <span style={{fontWeight: 300, fontSize: "0.8rem", flex: 1, textAlign: "left", paddingTop: "0.2rem", color: theme.primary.main}}>APOLLOCARE 2020 &copy;</span>
 	      </div>
   		</Grid>
   	);
